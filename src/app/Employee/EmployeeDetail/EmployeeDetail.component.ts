bea4df032b4e942a4f06ad766b004081d99e2fa3
Ramprit Sahani <Rampritsahani@outlook.in>
2017-03-12 12:56:49 +0530

    Changes

--- src/app/Employee/EmployeeDetail/EmployeeDetail.component.ts
+++ src/app/Employee/EmployeeDetail/EmployeeDetail.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../Employee.service'
 import { IEmployee } from "../Employee.model";
+import { Router } from '@angular/router'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   templateUrl: './EmployeeDetail.component.html',
   styleUrls: ['./EmployeeDetail.component.css']
 })
 export class EmployeeDetailComponent implements OnInit {
-
   employee: IEmployee
-  constructor(private employeeService: EmployeeService) {
-
+  constructor(private employeeService: EmployeeService,
+    private route: ActivatedRoute) {
   }
   ngOnInit() {
-    this.employee = this.employeeService.GetEmployee(4);
+    this.employee =
+      this.employeeService.
+        GetEmployee(+this.route.snapshot.params['Id']);
   }
 
 }
